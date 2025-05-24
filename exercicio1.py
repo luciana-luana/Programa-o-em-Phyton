f4a5a8376dddfef8271936370b55bd4ea492c6b5
Luciana da Cruz <lucianadacruz.unicv@gmail.com>
2025-05-24 23:52:21 +0100

    primeiro commit

---
+++ exercicio1.py
@@ -0,0 +1,14 @@
+import math 
+def calculo ():
+    a=(10-7)*(4-3)
+    return a
+
+    print("o resultado é:", calculo())
+
+def segundo():
+    b = 10 - 7 * 4 - 3
+    return b
+
+print("O resultado é:", segundo())
+
+
