e5840c0939fdc71c6794345950b1e75f2bb81d24
Luciana da Cruz <lucianadacruz.unicv@gmail.com>
2025-05-26 13:54:55 +0100

    segundo

--- exercicio2.py
+++ exercicio2.py
@@ -8,3 +8,36 @@ def d (x,y): #divisão inteira
     return x//y
 
 print(d(2,2))   
+
+def e(x, y):  # operador de módulo
+    return x % y
+print("O resultado é:", e(17, 3))
+
+def f():
+    return (3 + 5) / 2 * 3
+
+print("O resultado é:", f())
+
+def g():
+    return(3+5)/(2*3)
+print("O resultado é:", g())
+
+def h():
+    return(1+1/3)
+print("O resultado é:", h())
+
+def i():
+    return(1+1/3.0)
+print("O resultado é:", i())
+
+def j():
+    return(1+1//3)
+print("O resultado é:", j())
+
+def k():
+    return(2**3)
+print("O resultado é:", k())
+
+def l():
+    return(2.0**3)
+print("O resultado é:", l())
