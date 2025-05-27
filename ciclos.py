62bbbddb83930a3a8ee580411b8f39006853ec19
Luciana da Cruz <lucianadacruz.unicv@gmail.com>
2025-05-27 12:54:29 +0100

    ciclos

---
+++ ciclos.py
@@ -0,0 +1,29 @@
+    
+tempC = [-5, 0, 5, 10, 15, 20, 25]
+for nome in tempC:
+    mensg = "A temperatura é " + str(nome)
+    print(mensg)
+    
+  
+import math 
+for x in range (-5,26,5): #-5 onde começa, 26 vai até 25, a contagem é de 5 em 5
+    x="A temperatura é" + str(nome)
+    print(x)
+
+def crescente (n): #n função com números inteiros 
+    i=-5
+    while i<n:
+        print(i)
+        i=i+5
+    print('fim')
+
+import math
+for h in range (10,56,3):
+    print(h)
+    
+import math #ver isso 
+for h in range (10,56,3):
+    if h % 7 ==0:
+        break
+        print(h)
+print("fim")
