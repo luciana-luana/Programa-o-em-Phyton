0cc3496fe9992e37cc081e6ea1ebca91192f9df3
Luciana da Cruz <lucianadacruz.unicv@gmail.com>
2025-06-09 18:50:56 +0100

    string

--- string.py
+++ string.py
@@ -18,3 +18,46 @@ def inversa(txt):
 
 def palindromo (txt):
     return inversa(txt).upper()==txt.upper()
+
+def media (xs):
+    return soma (xs)/len(xs)
+    
+    
+def soma (sequencia):
+    resultado=0
+    for i in sequencia:
+        resultado=resultado+i
+    return resultado
+
+def produto (xs):
+    total=1
+    for x in xs:
+        total=total * x 
+    return total 
+
+def maximo (xs):
+    maior=xs[0]
+    for x in xs [1:]:
+        maior=max(maior,x)
+    return maior
+
+def elimrep1(xs):
+    ys=[]
+    for x in xs:
+        if not (x in ys):
+            ys.append(x)
+    return ys
+
+def elimrep2(xs):
+    i=0
+    while i<len(xs):
+        if xs[i] in xs[0:i]:
+            del xs[i]
+        else:
+            i=i+1
+
+
+
+
+xs = ys = -1;
+xs diferente ys - retona o menor indíce
